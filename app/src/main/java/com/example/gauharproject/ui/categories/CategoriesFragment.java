b017398dd9699fe10d003fb3f0f79b4a434116f1
Gauhar <gauharbaikadamova1999@gmail.com>
2020-04-04 15:06:05 +0300

    add category layout

--- app/src/main/java/com/example/gauharproject/ui/categories/CategoriesFragment.java
+++ app/src/main/java/com/example/gauharproject/ui/categories/CategoriesFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,11 +15,20 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.gauharproject.MainActivity;
 import com.example.gauharproject.R;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class CategoriesFragment extends Fragment {
 
     private CategoriesViewModel categoriesViewModel;
+    private String [] categories = { "Meditation essentials", "Falling sleep & waking up" , "Sport", "Beuty & Health", "Perfomance mindset" ,
+                                       "Work & Productivity", "Kids & Parenting" , "Cooking" , "Travel" , "Fashion"};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +42,21 @@ public class CategoriesFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        final ListView categoryList = root.findViewById(R.id.categoryList);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter (getActivity(), android.R.layout.simple_list_item_1,categories);
+        categoryList.setAdapter(adapter);
+        categoryList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = categories[position];
+//                selection.setText(selectedItem);
+            }
+        });
         return root;
+
+
+
     }
 }
