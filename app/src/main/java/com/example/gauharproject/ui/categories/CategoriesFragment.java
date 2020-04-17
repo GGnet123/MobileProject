e02dd245eb7dbe731c3a7654da3169e5f31a5212
Gauhar <gauharbaikadamova1999@gmail.com>
2020-04-17 21:26:52 +0300

    adding categories

--- app/src/main/java/com/example/gauharproject/ui/categories/CategoriesFragment.java
+++ app/src/main/java/com/example/gauharproject/ui/categories/CategoriesFragment.java
@@ -1,9 +1,11 @@
 package com.example.gauharproject.ui.categories;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -15,6 +17,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.gauharproject.CategoryActivity;
 import com.example.gauharproject.MainActivity;
 import com.example.gauharproject.R;
 
@@ -34,7 +37,7 @@ public class CategoriesFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         categoriesViewModel =
                 ViewModelProviders.of(this).get(CategoriesViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_categories, container, false);
+        final View root = inflater.inflate(R.layout.fragment_categories, container, false);
         final TextView textView = root.findViewById(R.id.text_categories);
         categoriesViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -43,20 +46,63 @@ public class CategoriesFragment extends Fragment {
             }
         });
 
-        final ListView categoryList = root.findViewById(R.id.categoryList);
+        final TextView cat1 = root.findViewById(R.id.cat1);
+        final TextView cat2 = root.findViewById(R.id.cat2);
+        final TextView cat3 = root.findViewById(R.id.cat3);
+        final TextView cat4 = root.findViewById(R.id.cat4);
+        final TextView cat5 = root.findViewById(R.id.cat5);
+        final TextView cat6 = root.findViewById(R.id.cat6);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter (getActivity(), android.R.layout.simple_list_item_1,categories);
-        categoryList.setAdapter(adapter);
-        categoryList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        cat1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String selectedItem = categories[position];
-//                selection.setText(selectedItem);
+            public void onClick(View v) {
+                Intent intent = new Intent((MainActivity)getContext(), CategoryActivity.class);
+                intent.putExtra("category", 1);
+                startActivity(intent);
             }
         });
+        cat2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent((MainActivity)getContext(), CategoryActivity.class);
+                intent.putExtra("category", 2);
+                startActivity(intent);
+            }
+        });
+        cat3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent((MainActivity)getContext(), CategoryActivity.class);
+                intent.putExtra("category", 3);
+                startActivity(intent);
+            }
+        });
+        cat4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent((MainActivity)getContext(), CategoryActivity.class);
+                intent.putExtra("category", 4);
+                startActivity(intent);
+            }
+        });
+        cat5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent((MainActivity)getContext(), CategoryActivity.class);
+                intent.putExtra("category", 5);
+                startActivity(intent);
+            }
+        });
+        cat6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent((MainActivity)getContext(), CategoryActivity.class);
+                intent.putExtra("category", 6);
+                startActivity(intent);
+            }
+        });
+
         return root;
-
-
 
     }
 }
