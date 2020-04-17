cdafec10544bf83965e8c27a853ae512f80e0e11
galym <galym@thefroot.com>
2020-04-18 03:31:32 +0600

    urls on images home

--- app/src/main/java/com/example/gauharproject/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/gauharproject/ui/home/HomeFragment.java
@@ -1,10 +1,13 @@
 package com.example.gauharproject.ui.home;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -21,7 +24,8 @@ import com.example.gauharproject.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-
+    private ImageView img1;
+    private ImageView img2;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
@@ -32,6 +36,29 @@ public class HomeFragment extends Fragment {
         ViewPager viewPager = root.findViewById(R.id.viewPager);
         ImageAdapter adapter = new ImageAdapter(((MainActivity)getActivity()));
         viewPager.setAdapter(adapter);
+
+        img1 = root.findViewById(R.id.blogImg);
+        img2 = root.findViewById(R.id.blogImg2);
+
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent viewIntent =
+                        new Intent("android.intent.action.VIEW",
+                                Uri.parse("https://www.lifehack.org/articles/lifestyle/100-life-hacks-that-make-life-easier.html"));
+                startActivity(viewIntent);
+            }
+        });
+
+        img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent viewIntent =
+                        new Intent("android.intent.action.VIEW",
+                                Uri.parse("https://www.rbc.ru/trends/education/5e727a799a79475f87486511"));
+                startActivity(viewIntent);
+            }
+        });
 
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
